2e8e9e773ce6b2ff7503ccdfc9f9d779dd07f340
Deep-Parekh <dparekh20@outlook.com>
2021-03-31 21:53:15 +0530

    Changed CustomUser model

--- thrift_store/users/models.py
+++ thrift_store/users/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 # from django.utils import timezone
 
 from .managers import CustomUserManager
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    username =  models.CharField(_('username'), max_length=20, blank=False, unique=True)
-    first_name = models.CharField(_('first name'), max_length=50, blank=True)
-    last_name = models.CharField(_('last name'), max_length=50, blank=True)
-    email = models.EmailField(_('email address'), unique=False)
-    # password = models.CharField(max_length=128, verbose_name='password')
+class CustomUser(AbstractUser, PermissionsMixin):
+    #username =  models.CharField(_('username'), max_length=20, blank=False, unique=True)
+    #first_name = models.CharField(_('first name'), max_length=50, blank=True)
+    #last_name = models.CharField(_('last name'), max_length=50, blank=True)
+    #email = models.EmailField(_('email address'), unique=False)
+    #password = models.CharField(max_length=128, verbose_name='password')
     contact_no = models.CharField(max_length=20, blank=False, default='')
     location = models.CharField(max_length=100, blank=False, default='')
     # products_listed
@@ -23,7 +23,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     # is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = ['email', 'username']
 
     objects = CustomUserManager()
 
